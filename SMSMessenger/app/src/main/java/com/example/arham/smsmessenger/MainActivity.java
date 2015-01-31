2f796da3278f6def76ff5112b96b3e3a7d470546
Arham Ahmed <a.arhamIT@gmail.com>
2015-01-31 16:26:17 -0500

    Added Geol-ocation Functionality
    Sends https request to get current location latitude/longtitude, fetches
    address/detailed info using Volley and Google GeoCode API

--- SMSMessenger/app/src/main/java/com/example/arham/smsmessenger/MainActivity.java
+++ SMSMessenger/app/src/main/java/com/example/arham/smsmessenger/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.arham.smsmessenger;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.os.Bundle;
-import android.app.Activity;
 import android.telephony.SmsManager;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HttpStack;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import com.android.volley.RequestQueue;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,6 +38,7 @@ public class MainActivity extends ActionBarActivity {
     Button sendBtn;
     EditText txtphoneNo;
     EditText txtMessage;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +48,71 @@ public class MainActivity extends ActionBarActivity {
         sendBtn = (Button) findViewById(R.id.btnSendSMS);
         txtphoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
         txtMessage = (EditText) findViewById(R.id.editTextSMS);
+
         sendBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 sendSMSMessage();
             }
         });
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+                 latitude+","+longitude+"&key="+"AIzaSyD_65i2dXl-bgq4Ei9ARkxTwg793I7_DV8";
+
+                decodeCoordinates(url);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+    }
+
+    public void decodeCoordinates(String url)
+    {
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        queue = new RequestQueue(cache, network);
+
+        // Start the queue
+        queue.start();
+
+        // Formulate the request and handle the response.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        txtMessage.setText(""+response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        txtMessage.setText("An error happened.");
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     protected void sendSMSMessage() {
